a2616ff9a9a421dea81bbdb31d6a9977ec40c084
ShogoMark <trigmark87@gmail.com>
2022-09-08 19:55:15 -0700

    taskall

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - writing a text in C
+ *
+ * Return: Always 0 (Successful)
+ *
+ */
+int main(void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+	}
