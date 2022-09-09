d40ab31b4cd376c98ad57b34037a2048ff2e4664
ShogoMark <trigmark87@gmail.com>
2022-09-08 20:00:52 -0700

    taskallb

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * main - Printing in C coding
  * Return: Always 0 (Successful)
  */
-int main (void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
