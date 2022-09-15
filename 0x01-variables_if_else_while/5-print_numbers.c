eea497245ac05faf857f2d0170e1ea7efe15f166
ShogoMark <trigmark87@gmail.com>
2022-09-15 08:56:51 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int f;
+
+	if (f = 0; f < 10; f++);
+		printf("%d", f);
+	printf("\n");
+	return (0);
+}
