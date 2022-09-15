54a686e1182af4eb91484cc3f015654ee6a168e3
ShogoMark <trigmark87@gmail.com>
2022-09-15 10:37:18 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	int lt;
+
+	for (lt = 'z'; lt <= 'a'; lt-)
+	{
+		putchar(lt);
+	}
+
+	putchar('\n');
+	return (0);
+}
