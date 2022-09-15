3348f78f667f2e359dd02d50c70fd4e11952bd9d
ShogoMark <trigmark87@gmail.com>
2022-09-15 10:12:36 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int f;
+
+	for (f = 48; f < 58; f++)
+	{
+		putchar(f);
+	}
+	putchar('\n')
+	return (0);
+}
