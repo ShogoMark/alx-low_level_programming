4b61466aef19a58c23a8eefc12955c25a4ff9b56
ShogoMark <trigmark87@gmail.com>
2022-09-15 15:42:47 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ *
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 10; a <= 19; a++)
+	{
+		for (b = 10; b <= 19; b++)
+		{
+			if ((b % 10) > (a % 10))
+			{
+				putchar((a % 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 18 || b != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
