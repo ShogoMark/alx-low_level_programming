ed6b0ab5afefb373767fe13722951e323d41e441
ShogoMark <trigmark87@gmail.com>
2022-09-15 11:17:51 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int f;
+	for (f = 0; f <= 9; f++)
+	{
+		putchar(f);
+		if (f != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
