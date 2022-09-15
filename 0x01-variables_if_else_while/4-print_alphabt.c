a0f7da138e19af3ae02fafb77d7371d0b7311bb2
ShogoMark <trigmark87@gmail.com>
2022-09-15 05:26:01 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lt = 'a';
+
+	while (ch <= 'z')
+	{
+		if (lt != 'e' && lt != 'q')
+		{
+			putchar(lt);
+		}
+		lt++;
+	}
+	putchar('\n');
+	return (0);
+}
