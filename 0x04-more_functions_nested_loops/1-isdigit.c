c6451ccf849366877fc9c3a9acc556c0a1afeccc
ShogoMark <trigmark87@gmail.com>
2022-09-16 10:53:47 -0700

    task1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 
 /**
  * _isdigit - checks if parammeter is a number between 0 - 9.
- * c: number to be checked
+ * @c: number to be checked
  * Return: 1 if c is a digit between 0 and 9, else 0
  */
 
