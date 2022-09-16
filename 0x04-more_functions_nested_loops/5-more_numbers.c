5614103a757e09a51ad9e9d6bf7fcc78018f8fb8
ShogoMark <trigmark87@gmail.com>
2022-09-16 13:45:33 -0700

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 - 14 10times
+ * followed by new line
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int n, lt;
+
+	for (n = 0; n < 10; n++)
+	{
+		if (lt = 0; lt < 15; lt++)
+		{
+			if (lt >= 10)
+				_putchar((lt / 10) + 48)
+			_putchar((lt % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
