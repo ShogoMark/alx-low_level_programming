6797e5f1830dba31f06ea228a09651cb324cbacc
ShogoMark <trigmark87@gmail.com>
2022-09-16 19:36:36 -0700

    task11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - prints the largest prime factorial of 612852475143
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned long int i = 3, n = 612852475143;
+
+	for (; i < 12057; i += 2)
+	{
+		while (n % i == 0 && n != i)
+			n /= i;
+	}
+	printf("%lu\n", n);
+
+	return (0);
+}	
