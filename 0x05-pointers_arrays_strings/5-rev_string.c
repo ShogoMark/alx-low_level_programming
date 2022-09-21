c64f23c97738f4cd198a44a8b767271b7dff4716
ShogoMark <trigmark87@gmail.com>
2022-09-20 20:53:06 -0700

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: supplied pointer argument for string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, max, half;
+	char a, z;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++
+	}
+	max = i - 1;
+	half = max / 2;
+	while (half >= 0)
+	{
+		a = s[max - half];
+		z = s[half];
+		s[half] = a;
+		s[max - half] = z;
+		half--;
+	}
+}
