30e91445602a53aeb7b8971fdcfb5e175eda503d
ShogoMark <trigmark87@gmail.com>
2022-09-20 19:54:04 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int: swaps the values of two integers
+ *
+ * @a: integer input 1
+ * @b: integer input 2
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b);
+{
+	int swp = *a;
+
+	*a = *b;
+
+	*b = swp;
+}
