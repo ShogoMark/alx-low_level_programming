d4c3c5b7792095e636f3c2de88ea12e6a431ee8d
ShogoMark <trigmark87@gmail.com>
2022-09-23 10:36:21 -0700

    task2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 
 /**
  * _strncat - concatenates two strings.
- * @dest: destination 
+ * @dest: destination
  * @src: source
  * @n: amount of bytes used from src.
  * Return: a pointer to the resulting string dest
