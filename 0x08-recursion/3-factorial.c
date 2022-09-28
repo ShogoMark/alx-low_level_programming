f9b42d009369f56e337282d5c4677f56ebdaa7a5
ShogoMark <trigmark87@gmail.com>
2022-09-28 13:30:27 -0700

    task4

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,20 +2,19 @@
 
 /**
  * int factorial - function that returns factoria of a number
- * @n: given number
- * Return: 0
+ * @n: int parameter
+ * Return: int
  */
 
 int factorial(int n)
 {
 	if (n < 0)
-	return -1;
 	{
-		if (n == 0)
-		     return 1;
-		{
-			_putchar(n * (n - 1));
-			n++;
-		}
+		return -1;
 	}
+	if (n == 1)
+	{
+		return 1;
+	}
+	return (n * factorial(n - 1));
 }
