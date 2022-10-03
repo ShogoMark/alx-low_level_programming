addd2f9608edf18a7301a777c4ff9e974732e112
ShogoMark <trigmark87@gmail.com>
2022-10-03 11:20:39 -0700

    task2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,16 +5,16 @@
  * main - prints all arguments in program
  * @argc: the argument count
  * @argv: the array that keeps arguments
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
-	printf("\n");
-
 	return (0);
 }
