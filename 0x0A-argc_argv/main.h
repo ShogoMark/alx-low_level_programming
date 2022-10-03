8d5a3b7b72c38942c2b5fbadcc474148ccad6faf
ShogoMark <trigmark87@gmail.com>
2022-10-03 10:57:02 -0700

    task2

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,11 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdio.h>
-
 int _putchar(char c);
 int main(int argc, char *argv[]);
 int main(int argc, char **argv);
-int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
