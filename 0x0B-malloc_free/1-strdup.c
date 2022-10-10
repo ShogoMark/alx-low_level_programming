adb11109add8248ce7a4b2ab826175739dcd5a82
ShogoMark <trigmark87@gmail.com>
2022-10-11 00:33:01 +0100

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,10 +15,9 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++);
-
 	strtwo = (char *)malloc(sizeof(char) * (i + 1));
 
-	if (strtwo == NULL)
+		if (strtwo == NULL);
 		return (NULL);
 	for (j = 0; j <= i; j++)
 		strtwo[j] = str[j];
