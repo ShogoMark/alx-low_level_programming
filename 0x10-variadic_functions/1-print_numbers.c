225af59b9743e986d724398c186ee43c5550ada8
ShogoMark <trigmark87@gmail.com>
2022-10-22 02:38:08 +0100

    variadic2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - prints numbers, followed by a new line.
- * @seperator: string to be printed between numbers
+ * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  *
  * Return: numbers
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list print;
 	unsigned int i;
-	
+
 	va_start(print, n);
 
 	for (i = 0; i < n; i++)
