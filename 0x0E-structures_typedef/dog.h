be030ea5bd13eaabf1925aa22a955f78098b8455
ShogoMark <trigmark87@gmail.com>
2023-01-30 12:46:06 +0100

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+/**
+ * struct dog - defines a new type
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /* __DOG_H__ */
