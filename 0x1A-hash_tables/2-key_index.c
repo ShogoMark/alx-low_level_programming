2764db6316c7d3c586b22dfe74a8fe3fc3f7cebb
ShogoMark <trigmark87@gmail.com>
2023-04-12 14:48:05 +0000

    task2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
 
